ea4ad1a6d257bfc83e7162fa236cbb63f5cccaae
davislipskis-oss <davis.lipskis@gmail.com>
2026-04-15 15:49:40 +0300

    Delete mtg_terminal_polished_final directory

--- mtg_terminal_polished_final/components/SectionHeading.js
+++
@@ -1,9 +0,0 @@
-export default function SectionHeading({ eyebrow, title, intro }) {
-  return (
-    <div className="section-heading">
-      {eyebrow ? <div className="eyebrow">{eyebrow}</div> : null}
-      <h2>{title}</h2>
-      {intro ? <p className="lead">{intro}</p> : null}
-    </div>
-  );
-}
